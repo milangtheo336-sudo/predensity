3e0fb03b5995af3630633a648205a38399dca573
i-mwangi <mwangihenry336@gmail.com>
2026-05-21 16:12:42 +0300

    Update frontend/recover-usdc.js

---
+++ frontend/recover-usdc.js
@@ -0,0 +1,59 @@
+// Recovery script: transfer USDC from the EVM alias account back to the sender
+// Run: node recover-usdc.js
+
+const { Client, PrivateKey, TransferTransaction, AccountId, TokenId } = require('@hashgraph/sdk');
+const crypto = require('crypto');
+
+// The encrypted private key from Convex managedWallets table
+const ENCRYPTED_KEY = 'a2a1d34974d985b8db196728e8d0dd70:2a0b35e7b7b22cbdbe3554c9972e462d:1973ee3f5302bf47cfea4537498ff7f48f75ed4c9dca17e8ac7ab982fae2fcfcc67aa573874911d6e05728ab431790a656e7254fdce20a46e3acd9c92599fdfc';
+const ENCRYPTION_KEY = process.env.WALLET_ENCRYPTION_KEY || 'predensity-dev-key-change-in-prod!!';
+
+// The alias account that received the USDC
+const ALIAS_ACCOUNT = '0.0.10405801';
+// Where to send it back
+const DESTINATION = '0.0.9968652';
+// USDC token ID on mainnet
+const USDC_TOKEN = '0.0.456858';
+// Amount in smallest unit (2 USDC = 2000000)
+const AMOUNT = 2000000;
+
+function decrypt(encryptedText) {
+  const key = crypto.scryptSync(ENCRYPTION_KEY, 'predensity-salt', 32);
+  const [ivHex, authTagHex, encrypted] = encryptedText.split(':');
+  const iv = Buffer.from(ivHex, 'hex');
+  const authTag = Buffer.from(authTagHex, 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv);
+  decipher.setAuthTag(authTag);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+async function main() {
+  // Decrypt the private key
+  const rawKey = decrypt(ENCRYPTED_KEY);
+  console.log('Decrypted key length:', rawKey.length);
+
+  const privateKey = PrivateKey.fromStringECDSA(rawKey);
+  console.log('Public key:', privateKey.publicKey.toStringRaw());
+
+  // The alias account uses the same key
+  const client = Client.forMainnet();
+  client.setOperator(ALIAS_ACCOUNT, privateKey);
+
+  console.log(`Transferring ${AMOUNT / 1e6} USDC from ${ALIAS_ACCOUNT} to ${DESTINATION}...`);
+
+  const tx = new TransferTransaction()
+    .addTokenTransfer(TokenId.fromString(USDC_TOKEN), AccountId.fromString(ALIAS_ACCOUNT), -AMOUNT)
+    .addTokenTransfer(TokenId.fromString(USDC_TOKEN), AccountId.fromString(DESTINATION), AMOUNT);
+
+  const response = await tx.execute(client);
+  const receipt = await response.getReceipt(client);
+
+  console.log('Status:', receipt.status.toString());
+  console.log('Transaction ID:', response.transactionId.toString());
+
+  client.close();
+}
+
+main().catch(console.error);
